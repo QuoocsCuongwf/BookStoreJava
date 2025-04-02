e4eb87995481cd74c5161cc441e1c0baa31e4f68
minhthu <thunguyenqn0610@gmail.com>
2025-04-02 16:50:51 +0700

    up

---
+++ src/main/java/com/example/demo/NhanVien.java
@@ -0,0 +1,27 @@
+//package com.example.demo;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import lombok.Data;
+//
+//import java.sql.Date;
+//
+//
+//@Entity
+//@Data
+//public class NhanVien {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY )
+//    private Integer id;
+//    private String ho;
+//    private String ten;
+//    private String chucVu;
+//    private int luong;
+//    private String cccd;
+//    private Date ngayVaoLam;;
+//    private String diaChi;
+//    private String soDienThoai;
+//
+//}
