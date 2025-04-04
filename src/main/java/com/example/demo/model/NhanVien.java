e7c55909d8f274c88f5ed7b4acd6062011da4c48
Quoocscuongwf <lequoccuong2204@gmail.com>
2025-04-04 10:10:44 +0700

    update gui nhanvien css

--- src/main/java/com/example/demo/model/NhanVien.java
+++ src/main/java/com/example/demo/model/NhanVien.java
@@ -1,5 +1,6 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -16,27 +17,35 @@ import java.time.LocalDate;
 public class NhanVien {
     @Id
     @Column(name = "MANV", nullable = false, length = 10)
+    @JsonProperty("MANV")
     private String manv;
 
     @Column(name = "HONV", nullable = false, length = 50)
+    @JsonProperty("HONV")
     private String honv;
 
     @Column(name = "TENNV", nullable = false, length = 30)
+    @JsonProperty("TENNV")
     private String tennv;
 
     @Column(name = "CHUCVU", length = 30)
+    @JsonProperty("CHUCVU")
     private String chucvu;
 
     @Column(name = "CCCD", length = 15)
+    @JsonProperty("CCCD")
     private String cccd;
 
     @Column(name = "LUONG")
+    @JsonProperty("LUONG")
     private Integer luong;
 
     @Column(name = "NGAYVAOLAM")
+    @JsonProperty("NGAYVAOLAM")
     private LocalDate ngayvaolam;
 
     @Column(name = "MAIL", length = 30)
+    @JsonProperty("MAIL")
     private String mail;
 
 }
