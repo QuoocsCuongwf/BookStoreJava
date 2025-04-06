286951ca52e60920eca90fffa08f2c68b29d9cb4
duyen124 <myduen3706@gmail.com>
2025-04-06 15:00:36 +0700

    fxml KHACHHANG : controller van chưa xong

--- src/main/java/com/example/demo/controller/KhachHangController.java
+++ src/main/java/com/example/demo/controller/KhachHangController.java
@@ -5,14 +5,25 @@ import com.example.demo.model.NhanVien;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class KhachHangController {
     @FXML
     private ObservableList<NhanVien> data;
+
+    @FXML
+    private AnchorPane khachHangAnchorPane;
+
+    @FXML
+    private Pane inforContainerKhachHang;
 
     @FXML
     private TableView<?> tableView;
@@ -32,8 +43,26 @@ public class KhachHangController {
             btn_timKiemKhachHang,btn_danhSachKhachHang;
 
 
+    public void initialize() {
+        inforContainerKhachHang.setVisible(false);
+    }
     //
     public void themKhachHang(ActionEvent actionEvent) {}
+
+    public void openInforContainerKhachHang() {
+        inforContainerKhachHang.setVisible(true);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/KhachHang.fxml"));
+        Scene scene = new Scene(loader.load());
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+
 
 
 
