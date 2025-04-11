8b6fd2bb305455a7a798f1594d98d230952aa2ee
minhthu <thunguyenqn0610@gmail.com>
2025-04-11 15:35:47 +0700

    insert delete search tacgia

--- src/main/java/com/example/demo/BUS/services/TacGiaServices.java
+++ src/main/java/com/example/demo/BUS/services/TacGiaServices.java
@@ -1,9 +1,9 @@
-package com.example.demo.services;
+package com.example.demo.BUS.services;
 
 import com.example.demo.databaseAccesssObject.TacGiaDAO;
-import com.example.demo.model.NhanVien;
 import com.example.demo.model.TacGia;
-import com.example.demo.spring_controller.TacGiaControllerSping;
+import com.example.demo.model.TacGia;
+import com.example.demo.BUS.spring_controller.TacGiaControllerSping;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
