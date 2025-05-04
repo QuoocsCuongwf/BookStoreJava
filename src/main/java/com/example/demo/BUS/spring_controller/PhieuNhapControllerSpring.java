117821717037b6a7951f1d15fa1ee62f3b7cea9b
duyen124 <myduen3706@gmail.com>
2025-05-04 17:20:37 +0700

    4.5 : PHIEUNHAP xong 90% ,còn controller chưa xong

---
+++ src/main/java/com/example/demo/BUS/spring_controller/PhieuNhapControllerSpring.java
@@ -0,0 +1,36 @@
+package com.example.demo.BUS.spring_controller;
+
+import com.example.demo.BUS.services.PhieuNhapServices;
+import com.example.demo.model.PhieuNhap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+
+@RequestMapping("/phieuNhap")
+public class PhieuNhapControllerSpring {
+    PhieuNhapServices phieuNhapServices=new PhieuNhapServices();
+
+    @GetMapping("/getAllPhieuNhap")
+    public List<PhieuNhap> getAllPhieuNhap() {
+        System.out.println("PhieuNhapControllerSpring:"+phieuNhapServices.getPhieuNhapList());
+        return  phieuNhapServices.getPhieuNhapList();
+    }
+    @PostMapping("/Search")
+    public List<PhieuNhap> timKiem(@RequestParam String find) {
+        return phieuNhapServices.searchPhieuNhap(find);
+    }
+    @PostMapping("/Add")
+    public String addPhieuNhap(@RequestBody PhieuNhap p) {
+        return phieuNhapServices.addPhieuNhap(p);// return success hoặc fail
+    }
+    @PostMapping("/Delete")
+    public String deletePhieuNhap(@RequestParam String maPhieuNhap) {
+        return phieuNhapServices.deletePhieuNhap(maPhieuNhap);
+    }
+    @PostMapping("/Update")
+    public String updatePhieuNhap(@RequestBody PhieuNhap p) {
+        return phieuNhapServices.updatePhieuNhap(p);
+    }
+}
